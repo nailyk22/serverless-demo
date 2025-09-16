137f51d130af13652eaceadd3a55238ad50bd4c8
kylian.gilbert <kylian.gilbert@epita.fr>
2025-09-16 11:05:45 +0200

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,13 +46,6 @@ resource "aws_lambda_function" "api" {
 
 data "aws_dynamodb_table" "contacts" {
   name         = "contacts"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
-
-  attribute {
-    name = "id"
-    type = "S"
-  }
 }
 
 resource "aws_api_gateway_rest_api" "api" {
